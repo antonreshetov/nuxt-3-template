118d6b787a8ef15ca5b29373619145065936d933
Anton Reshetov <reshetov.art@gmail.com>
2024-01-15 23:36:11 +0300

    feat: add prettier settings & update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
-    'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
     'vue/max-attributes-per-line': [
       'error',
       {
         singleline: 1,
       },
     ],
+  },
+  formatters: {
+    css: true,
   },
 })
