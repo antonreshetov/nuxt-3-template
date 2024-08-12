6585347b93a96f0c6fb66a389eaf12ddd2862bfb
Anton Reshetov <reshetov.art@gmail.com>
2024-08-12 16:31:14 +0300

    build(nuxt): add compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-12',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   tailwindcss: {
